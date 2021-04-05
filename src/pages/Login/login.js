aa2a25822e494cf0f772dc36207fd30d12d77c2f
Mariana Helena <marianahines@hotmail.com>
2021-04-05 19:07:50 -0300

    adding images and login interface

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,8 +1,50 @@
 import React from 'react';
+import background from "../../images/Wallpaper.jpg";
+import logo from "../../images/Logo.png";
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import useStyles from './styles.js';
 
 export default function Login() {
 
+    const styles = useStyles();
+
     return (
-        <p>oi</p>
+        <div style={{ 
+            backgroundImage: `url(${background})` ,
+            width:'100%',
+            height:'auto',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover'}}>
+                <div style={{height:'100px'}}> </div>
+                <Container maxWidth="sm"  style={{ 
+                    width:'500px',
+                    height:'500px',
+                    backgroundColor:'white',
+                    padding: 25
+                    }}>
+                <img src={logo} style={{width:'150px',  display: 'block',
+                marginLeft: 'auto', marginRight: 'auto'}}/> 
+                <div style={{display: 'block',
+                marginLeft: 'auto', marginRight: 'auto', textAlign:'center'}}>
+                    <span className={styles.titulo}> Login </span>
+                </div>
+                
+                <TextField id="outlined-basic" label="E-mail" variant="outlined" />
+                <TextField id="outlined-basic" label="Senha" variant="outlined" type="password"/>
+
+                <Button >
+                    Esqueceu a senha?
+                </Button>
+                <Button variant="contained" color="primary">
+                    Entrar
+                </Button>
+                <Button variant="contained" >
+                    Cadastrar
+                </Button>
+                </Container>
+                <div style={{height:'150px'}}> </div>
+        </div>
     );
 }
---
+++ src/pages/Login/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  titulo: {
+    color:'#1C587C', 
+    fontSize:'41px'
+  },
+}));
+
+export default useStyles;
