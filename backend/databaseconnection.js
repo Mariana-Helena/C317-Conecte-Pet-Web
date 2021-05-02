fac2374df7acbf2ee23f66de54fc40236956e5ea
Mariana Helena <marianahines@hotmail.com>
2021-05-01 21:59:10 -0300

    removing unused files

--- backend/databaseconnection.js
+++
@@ -1,5 +0,0 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://ConectePet:C317ConectePet@clusterc317.bnkbh.mongodb.net/ConectePet?retryWrites=true&w=majority";
-export const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
