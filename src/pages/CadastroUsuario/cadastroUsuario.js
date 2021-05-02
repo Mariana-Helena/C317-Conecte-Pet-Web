13061e792855c54dd7c2563770e40e7f9639cd47
Mariana Helena <marianahines@hotmail.com>
2021-05-02 13:57:05 -0300

    changing field type

--- src/pages/CadastroUsuario/cadastroUsuario.js
+++ src/pages/CadastroUsuario/cadastroUsuario.js
@@ -217,6 +217,7 @@ export default function CadastroUsuario() {
                         />
                         <div></div>
                         <TextField
+                            type= 'password'
                             id="senha"
                             label="Senha"
                             variant="outlined"
